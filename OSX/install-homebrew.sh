034e9cc60edb35efeea037c5ff09d56242a5fdda
Kevin Ushey <kevinushey@gmail.com>
2015-04-05 04:43:05 -0700

    use dsym

--- OSX/install-homebrew.sh
+++ OSX/install-homebrew.sh
@@ -32,6 +32,9 @@
 : ${MAKE:=make}
 : ${MAKEFLAGS:=-j10}
 
+## Ensure .dSYM folders are generated, to ensure easier debugging
+export PKG_MAKE_DSYM=yes
+
 ## ----- END CONFIGURATION VARIABLES ----- ##
 
 OWD=`pwd`
